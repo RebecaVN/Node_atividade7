786f6bf1e29c21bb53c10417b4455e5fa35ff4b4
Rebeca Nunes <rebecaviananunes@gmail.com>
2023-09-11 19:08:33 -0300

    Add files via upload

---
+++ calculos.js
@@ -0,0 +1,12 @@
+//Importando o módulo 
+const readline = require('readline-sync')
+
+const num1 = parseInt(readline.question("Digite o primeiro numero: \n"))
+const num2 = parseInt(readline.question("Digite o segundo numero: \n"))
+
+
+console.log("A divisão é: " + num1/num2);
+
+console.log(`A soma é: ` + (num1+num2));
+console.log(`A subtração é: ` + (num1-num2));
+console.log("A multiplicação é: " + num1*num2);
